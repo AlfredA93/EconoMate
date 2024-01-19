54cc55786501b401688ccf6f49edfa93d735782c
BanditCook <tanguy.lalexandre@gmail.com>
2024-01-19 10:31:16 +0000

    Took percentages of the sliders and put them in a paragraph

--- sliders.js
+++ sliders.js
@@ -3,6 +3,7 @@ import { calculateTotal, toggleLock, adjustSliders, displaySliderValues } from '
 const allSliders = Array.from(document.querySelectorAll('.slider'));
 const lockButtons = document.querySelectorAll('.lockButton');
 const totalBudgetInput = document.querySelector('#totalBudget');
+let percentagesParagraph = document.querySelector('.percentages');
 let unlockedSliders = [...allSliders];
 let lockedSliders = [];
 
@@ -15,6 +16,6 @@ lockButtons.forEach((lockButton, index) => {
 allSliders.forEach(slider => {
     slider.addEventListener('input', function() {
         adjustSliders(this, unlockedSliders, totalBudgetInput);
-        displaySliderValues(allSliders, totalBudgetInput);
+        displaySliderValues(allSliders, totalBudgetInput,percentagesParagraph);
     });
 });
